8ed2d4796afbadb594e722e7ce137a433ceaccdb
Michael Paquier <michael@paquier.xyz>
2025-06-18 16:23:16 +0900

    Introduce global 64-bit TOAST ID counter in control file
    An 8 byte counter is added to the control file, providing a unique
    64-bit-wide source for toast value IDs, with the same guarantees as OIDs
    in terms of durability.  SQL functions and tools looking at the control
    file are updated.  A WAL record is generated every 8k values generated,
    that can be adjusted if required.
    
    Requires a bump of WAL format.
    Requires a bump of control file version.
    Requires a catalog version bump.

---
+++ src/backend/access/common/toast_counter.c
@@ -0,0 +1,98 @@
+/*-------------------------------------------------------------------------
+ *
+ * toast_counter.c
+ *	  Functions for TOAST value counter.
+ *
+ * Copyright (c) 2025, PostgreSQL Global Development Group
+ *
+ *
+ * IDENTIFICATION
+ *	  src/backend/access/common/toast_counter.c
+ *
+ *-------------------------------------------------------------------------
+ */
+#include "postgres.h"
+
+#include "access/toast_counter.h"
+#include "access/xlog.h"
+#include "miscadmin.h"
+#include "storage/lwlock.h"
+#include "storage/shmem.h"
+
+/* Number of TOAST values to preallocate before WAL work */
+#define TOAST_ID_PREFETCH		8192
+
+/* pointer to variables struct in shared memory */
+ToastCounterData *ToastCounter = NULL;
+
+/*
+ * Initialization of shared memory for ToastCounter.
+ */
+Size
+ToastCounterShmemSize(void)
+{
+	return sizeof(ToastCounterData);
+}
+
+void
+ToastCounterShmemInit(void)
+{
+	bool		found;
+
+	/* Initialize shared state struct */
+	ToastCounter = ShmemInitStruct("ToastCounter",
+								   sizeof(ToastCounterData),
+								   &found);
+	if (!IsUnderPostmaster)
+	{
+		Assert(!found);
+		memset(ToastCounter, 0, sizeof(ToastCounterData));
+	}
+	else
+		Assert(found);
+}
+
+/*
+ * GetNewToastId
+ *
+ * Toast IDs are generated as a cluster-wide counter.  They are 64 bits
+ * wide, hence wraparound will unlikely happen.
+ */
+uint64
+GetNewToastId(void)
+{
+	uint64		result;
+
+	if (RecoveryInProgress())
+		elog(ERROR, "cannot assign TOAST IDs during recovery");
+
+	LWLockAcquire(ToastIdGenLock, LW_EXCLUSIVE);
+
+	/*
+	 * Check for initialization or wraparound of the toast counter ID.
+	 * InvalidToastId (0) should never be returned.  We are 64 bit-wide, hence
+	 * wraparound is unlikely going to happen, but this check is cheap so
+	 * let's play it safe.
+	 */
+	if (ToastCounter->nextId < ((uint64) FirstToastId))
+	{
+		/* Most-likely first bootstrap or initdb assignment */
+		ToastCounter->nextId = FirstToastId;
+		ToastCounter->idCount = 0;
+	}
+
+	/* If running out of logged for TOAST IDs, log more */
+	if (ToastCounter->idCount == 0)
+	{
+		XLogPutNextToastId(ToastCounter->nextId + TOAST_ID_PREFETCH);
+		ToastCounter->idCount = TOAST_ID_PREFETCH;
+	}
+
+	result = ToastCounter->nextId;
+	(ToastCounter->nextId)++;
+	(ToastCounter->idCount)--;
+
+	LWLockRelease(ToastIdGenLock);
+
+	return result;
+}
--- src/backend/access/transam/xlog.c
+++ src/backend/access/transam/xlog.c
@@ -53,6 +53,7 @@
 #include "access/rewriteheap.h"
 #include "access/subtrans.h"
 #include "access/timeline.h"
+#include "access/toast_counter.h"
 #include "access/transam.h"
 #include "access/twophase.h"
 #include "access/xact.h"
@@ -5269,6 +5270,7 @@ BootStrapXLOG(uint32 data_checksum_version)
 	checkPoint.nextOid = FirstGenbkiObjectId;
 	checkPoint.nextMulti = FirstMultiXactId;
 	checkPoint.nextMultiOffset = 0;
+	checkPoint.nextToastId = FirstToastId;
 	checkPoint.oldestXid = FirstNormalTransactionId;
 	checkPoint.oldestXidDB = Template1DbOid;
 	checkPoint.oldestMulti = FirstMultiXactId;
@@ -5281,6 +5283,10 @@ BootStrapXLOG(uint32 data_checksum_version)
 	TransamVariables->nextXid = checkPoint.nextXid;
 	TransamVariables->nextOid = checkPoint.nextOid;
 	TransamVariables->oidCount = 0;
+
+	ToastCounter->nextId = checkPoint.nextToastId;
+	ToastCounter->idCount = 0;
+
 	MultiXactSetNextMXact(checkPoint.nextMulti, checkPoint.nextMultiOffset);
 	AdvanceOldestClogXid(checkPoint.oldestXid);
 	SetTransactionIdLimit(checkPoint.oldestXid, checkPoint.oldestXidDB);
@@ -5757,6 +5763,8 @@ StartupXLOG(void)
 	TransamVariables->nextXid = checkPoint.nextXid;
 	TransamVariables->nextOid = checkPoint.nextOid;
 	TransamVariables->oidCount = 0;
+	ToastCounter->nextId = checkPoint.nextToastId;
+	ToastCounter->idCount = 0;
 	MultiXactSetNextMXact(checkPoint.nextMulti, checkPoint.nextMultiOffset);
 	AdvanceOldestClogXid(checkPoint.oldestXid);
 	SetTransactionIdLimit(checkPoint.oldestXid, checkPoint.oldestXidDB);
@@ -7299,6 +7307,12 @@ CreateCheckPoint(int flags)
 		checkPoint.nextOid += TransamVariables->oidCount;
 	LWLockRelease(OidGenLock);
 
+	LWLockAcquire(ToastIdGenLock, LW_SHARED);
+	checkPoint.nextToastId = ToastCounter->nextId;
+	if (!shutdown)
+		checkPoint.nextToastId += ToastCounter->idCount;
+	LWLockRelease(ToastIdGenLock);
+
 	MultiXactGetCheckptMulti(shutdown,
 							 &checkPoint.nextMulti,
 							 &checkPoint.nextMultiOffset,
@@ -8239,6 +8253,22 @@ XLogPutNextOid(Oid nextOid)
 }
 
 /*
+ * Write a NEXT_TOAST_ID log record.
+ */
+void
+XLogPutNextToastId(uint64 nextId)
+{
+	XLogBeginInsert();
+	XLogRegisterData(&nextId, sizeof(uint64));
+	(void) XLogInsert(RM_XLOG_ID, XLOG_NEXT_TOAST_ID);
+
+	/*
+	 * The next TOAST value ID is not flushed immediately, for the same reason
+	 * as above for the OIDs in XLogPutNextOid().
+	 */
+}
+
+/*
  * Write an XLOG SWITCH record.
  *
  * Here we just blindly issue an XLogInsert request for the record.
@@ -8453,6 +8483,16 @@ xlog_redo(XLogReaderState *record)
 		TransamVariables->oidCount = 0;
 		LWLockRelease(OidGenLock);
 	}
+	else if (info == XLOG_NEXT_TOAST_ID)
+	{
+		uint64		nextToastId;
+
+		memcpy(&nextToastId, XLogRecGetData(record), sizeof(uint64));
+		LWLockAcquire(ToastIdGenLock, LW_EXCLUSIVE);
+		ToastCounter->nextId = nextToastId;
+		ToastCounter->idCount = 0;
+		LWLockRelease(ToastIdGenLock);
+	}
 	else if (info == XLOG_CHECKPOINT_SHUTDOWN)
 	{
 		CheckPoint	checkPoint;
@@ -8467,6 +8507,10 @@ xlog_redo(XLogReaderState *record)
 		TransamVariables->nextOid = checkPoint.nextOid;
 		TransamVariables->oidCount = 0;
 		LWLockRelease(OidGenLock);
+		LWLockAcquire(ToastIdGenLock, LW_EXCLUSIVE);
+		ToastCounter->nextId = checkPoint.nextToastId;
+		ToastCounter->idCount = 0;
+		LWLockRelease(ToastIdGenLock);
 		MultiXactSetNextMXact(checkPoint.nextMulti,
 							  checkPoint.nextMultiOffset);
 
