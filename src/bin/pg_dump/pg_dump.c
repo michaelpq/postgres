0f955879bf80f6274d0770466e25f87361a46d6b
Michael Paquier <michael@paquier.xyz>
2025-08-14 10:57:59 +0900

    Add support for TOAST chunk_id type in binary upgrades
    This commit adds a new function, which would set the type of a chunk_id
    attribute for a TOAST table across upgrades.  This piece currently works
    only with chunk_id = OIDOID, but it is required in a follow-up patch
    where support for chunk_id = OID8OID is supported on top of the existing
    one.

--- src/bin/pg_dump/pg_dump.c
+++ src/bin/pg_dump/pg_dump.c
@@ -104,6 +104,7 @@ typedef struct
 	RelFileNumber relfilenumber;	/* object filenode */
 	Oid			toast_oid;		/* toast table OID */
 	RelFileNumber toast_relfilenumber;	/* toast table filenode */
+	Oid			toast_chunk_id_typoid;	/* type of chunk_id attribute */
 	Oid			toast_index_oid;	/* toast table index OID */
 	RelFileNumber toast_index_relfilenumber;	/* toast table index filenode */
 } BinaryUpgradeClassOidItem;
@@ -5918,7 +5919,10 @@ collectBinaryUpgradeClassOids(Archive *fout)
 	const char *query;
 
 	query = "SELECT c.oid, c.relkind, c.relfilenode, c.reltoastrelid, "
-		"ct.relfilenode, i.indexrelid, cti.relfilenode "
+		"ct.relfilenode, i.indexrelid, cti.relfilenode, "
+		"(SELECT a.atttypid FROM pg_attribute AS a "
+		"  WHERE a.attrelid = c.reltoastrelid AND attname = 'chunk_id'::text) "
+		"  AS toastchunktypid "
 		"FROM pg_catalog.pg_class c LEFT JOIN pg_catalog.pg_index i "
 		"ON (c.reltoastrelid = i.indrelid AND i.indisvalid) "
 		"LEFT JOIN pg_catalog.pg_class ct ON (c.reltoastrelid = ct.oid) "
@@ -5940,6 +5944,7 @@ collectBinaryUpgradeClassOids(Archive *fout)
 		binaryUpgradeClassOids[i].toast_relfilenumber = atooid(PQgetvalue(res, i, 4));
 		binaryUpgradeClassOids[i].toast_index_oid = atooid(PQgetvalue(res, i, 5));
 		binaryUpgradeClassOids[i].toast_index_relfilenumber = atooid(PQgetvalue(res, i, 6));
+		binaryUpgradeClassOids[i].toast_chunk_id_typoid = atooid(PQgetvalue(res, i, 7));
 	}
 
 	PQclear(res);
@@ -6004,6 +6009,9 @@ binary_upgrade_set_pg_class_oids(Archive *fout,
 			appendPQExpBuffer(upgrade_buffer,
 							  "SELECT pg_catalog.binary_upgrade_set_next_toast_relfilenode('%u'::pg_catalog.oid);\n",
 							  entry->toast_relfilenumber);
+			appendPQExpBuffer(upgrade_buffer,
+							  "SELECT pg_catalog.binary_upgrade_set_next_toast_chunk_id_typoid('%u'::pg_catalog.oid);\n",
+							  entry->toast_chunk_id_typoid);
 
 			/* every toast table has an index */
 			appendPQExpBuffer(upgrade_buffer,
